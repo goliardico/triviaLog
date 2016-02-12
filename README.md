e58230220897f2c58cf0def5dd54d08cfcda0c02
Paolo <goliardico@gmail.com>
2016-02-12 17:37:33 +0100

    First release. Output to console, file works. Missing network.

---
+++ README.md
@@ -0,0 +1,66 @@
+## TriviaLog
+
+Build on top of console.log, TriviaLog don't use any external dependencies trying to be the smallest as possible.
+
+## Code Example
+
+After installation, include the library and print some messages:
+
+```javascript
+tl = require('triviaLog');
+
+// Set log limit to WARN (don't print everthing above)
+tl.setParam('limit', 'WARN');
+
+tl.log('ALERT', 'This will be printed to console.');
+tl.log('DEBUG', 'This will NOT be shown to console because DEBUG level is above ALERT.');
+
+// Print also to a log file
+tl.setParam('outFile', './test.log');
+
+tl.log('ALERT', 'write to file', {f1: 100, f2: 'It is too late', f3: true});
+
+```
+
+## Motivation
+
+I made this just to test and experiment with npm module creation, but it's works and I'll use it.
+
+## Installation
+
+TODO: I don't have already publish to npm...
+
+Install as regular npm module:
+
+```sh
+
+npm install triviaLog
+
+```
+
+## API Reference
+
+Use .setParam to modify default parameter or set output file.
+
+Use .getParam to read value of param.
+
+List of available parameters:
+* limit: set log limit (use next table reference)
+* outFile: set an output file
+* outNet: set a network destination, UDP (TODO)
+
+### LogLevels
+````javascript
+logLevels.set('EMER',    0);
+logLevels.set('ALERT',   1);
+logLevels.set('CRIT'    ,2);
+logLevels.set('ERR',     3);
+logLevels.set('WARN',    4);
+logLevels.set('NOTICE',  5);
+logLevels.set('INFO',    6);
+logLevels.set('DEBUG',   7);
+````
+
+## License
+
+Released under MIT License (see LICENSE file).
---
+++ triviaLog.js
@@ -0,0 +1,121 @@
+// Minimum dependency (or even zero) logger facility.
+// TODO:
+// define external destination (file, network) DONE (file)
+// create (optionally) a web server who send last "n" log entries
+
+'use strict';
+var triviaLog = {};
+
+const util = require('util');
+const fs   = require('fs');
+
+// Default display option for Terminal output
+const mesgDisplayOptions = {
+	showHidden: false,
+	depth: null,
+	colors: true
+}
+
+// Default inspect option for File/Newtork output
+const mesgFileOptions = {
+	showHidden: false,
+	depth: null,
+	colors: false
+}
+
+
+// Define a Map (ES6) with all values available for log levels
+const logLevels = new Map();
+
+logLevels.set('EMER',    0);
+logLevels.set('ALERT',   1);
+logLevels.set('CRIT'    ,2);
+logLevels.set('ERR',     3);
+logLevels.set('WARN',    4);
+logLevels.set('NOTICE',  5);
+logLevels.set('INFO',    6);
+logLevels.set('DEBUG',   7);
+
+
+// Parameters for logging. Default to INFO
+var logParams = new Map();
+
+logParams.set('limit', 'INFO');
+logParams.set('outFile', null);
+logParams.set('outNet', null);
+
+
+// ---
+function formatDate() {
+	let d = new Date();
+	return d.getFullYear() + '-' + ('0' + (d.getMonth()+1)).slice(-2) + '-'
+		+ ('0' + d.getDate()).slice(-2) + ' ' + ('0' + d.getHours()).slice(-2) + ':'
+		+ ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2) + '.'
+		+ d.getMilliseconds();
+}
+
+
+triviaLog.log = function() {
+	let logLevel = 'INFO';
+	let msg = '';
+	let msgObj = '';
+	let msgObjFile = '';
+
+	for ( let pos = 0; pos < arguments.length; pos++ ) {
+		// Check if first argument is a level-reserved word (see logLevels Map above).
+		if ( pos === 0 && logLevels.has(arguments[0]) )
+			logLevel = arguments[0];
+		else {
+			if ( typeof arguments[pos] === 'string' )
+				msg = msg + ' ' + arguments[pos];
+			if ( typeof arguments[pos] === 'object' ) {
+				msgObj = msgObj + ' - ' + util.inspect(arguments[pos], mesgDisplayOptions);
+				msgObjFile = msgObjFile + ' - ' + util.inspect(arguments[pos], mesgFileOptions);
+			}
+		}
+	}
+
+	// Output only if logLevel is equal or less than the actual limit (logParams)
+	if (logLevels.get(logLevel) <= logLevels.get(logParams.get('limit')) ) {
+		let d = formatDate();
+		// Log to console (always)
+		console.log(d + ' ['+logLevel+']' + msg + msgObj);
+		// Then try if defined an output to file
+		if ( typeof logParams.get('outFile') === 'string' )
+			fs.appendFile(logParams.get('outFile'), d + ' ['+logLevel+']' + msg + msgObjFile +'\n', (err) => {
+				if (err) throw err;
+			});
+	}
+}
+
+// Set logParams (with parameter name)
+triviaLog.setParam = function(param, value) {
+
+	const err = 'Something wrong with .setParam: ' + param;
+
+	// If param name is invalid (not string or non exists)
+	if (typeof param !== 'string' || !logParams.has(param)) throw err;
+	switch (param) {
+		case 'limit':
+		if (logLevels.has(value))
+			logParams.set(param,value);
+		break;
+		case 'outFile':
+			// Write a message to file, if Ok, set parameter and use it for outputs
+			// Use sync version (only here) because I need to wait for disk before set logParams
+			fs.appendFileSync(value, formatDate() + ' Starting...\n', 'utf8');
+			logParams.set(param,value);
+		break;
+		default:
+			logParams.set(param, value);
+	}
+
+}
+
+// Show Parameter value
+triviaLog.getParam = function(param) {
+	if (typeof param === 'string' && logParams.has(param))
+		return logParams.get(param);
+}
+
+module['exports'] = triviaLog;
